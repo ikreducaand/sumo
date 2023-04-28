b4eb4167f0c3ac9771777286a561b28a7df20f50
ikreducaand <132067470+ikreducaand@users.noreply.github.com>
2023-04-28 14:15:13 +0200

    arreglado v.1.2

--- main.ts
+++ main.ts
@@ -1,26 +1,19 @@
 let DISTANCIA = 0
-let strip: neopixel.Strip = null
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     DISTANCIA = maqueen.Ultrasonic(PingUnit.Centimeters)
     basic.pause(500)
-    if (DISTANCIA >= 1) {
+    if (DISTANCIA > 1 && DISTANCIA <= 20) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
     } else {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
         basic.pause(2000)
     }
-})
-basic.forever(function () {
-    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(500)
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-    basic.pause(500)
-    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-    basic.pause(500)
 })
